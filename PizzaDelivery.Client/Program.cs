12461f68716c4c6b7720a57eadea7c2583d7249d
sudeep baniya <sudeep.cb@gmail.com>
2019-08-12 15:39:13 -0500

    all folders for pizzadelivery

---
+++ PizzaDelivery.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaDelivery.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
